e90aa7febd2aa49dade50829bdaa7f9e2fdbb215
anon <n/a>
2020-12-11 08:44:30 +0100

    prepare day 12

---
+++ day12/src/main.rs
@@ -0,0 +1,11 @@
+use common::input::Input;
+
+fn main() {
+    let input = Input::from_file("data/day12-input.txt");
+
+    let _ = input.iter_lines()
+        .inspect(|x| println!("{:?}", x))
+        .collect::<Vec<_>>();
+
+    println!("Hello, world!");
+}
