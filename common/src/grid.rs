98618e600ac76e59c09314adda631e9e77771d04
anon <n/a>
2020-12-11 08:41:23 +0100

    day 11

--- common/src/grid.rs
+++ common/src/grid.rs
@@ -1,7 +1,7 @@
 use crate::input::Input;
 use std::any::type_name;
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq, Eq)]
 pub struct Grid<T> {
     data: Vec<T>,
     pub width: usize,
@@ -27,6 +27,65 @@ impl<T> Grid<T> {
         debug_assert!(row < self.height);
         debug_assert!(col < self.width);
         &self.data[row * self.width + col]
+    }
+
+    pub fn safe_get(&self, row: isize, col: isize) -> Option<&T> {
+        if row >= self.height as isize || col >= self.width as isize || row < 0 || col < 0 {
+            return None;
+        }
+        Some(&self.data[row as usize * self.width + col as usize])
+    }
+
+    pub fn set(&mut self, row: usize, col: usize, val: T) {
+        debug_assert!(row < self.height);
+        debug_assert!(col < self.width);
+        self.data[row * self.width + col] = val
+    }
+
+    pub fn swap(a: &mut Self, b: &mut Self) {
+        std::mem::swap(a, b)
+    }
+
+    pub fn flat_iter(&self) -> impl Iterator<Item = &T> {
+        self.data.iter()
+    }
+
+    pub fn trace(
+        &self,
+        row_start: usize,
+        col_start: usize,
+        row_step: isize,
+        col_step: isize,
+    ) -> impl Iterator<Item = &T> {
+        assert!(row_step != 0 || col_step != 0);
+
+        let w = self.width as isize;
+        let h = self.height as isize;
+
+        //if row_start == 2 && col_start == 2 {println!("{},{}", row_step, col_step)}
+
+        (0..)
+            .map(move |t| {
+                (
+                    row_start as isize + t * row_step,
+                    col_start as isize + t * col_step,
+                )
+            })
+            //.inspect(move |x| if row_start == 2 && col_start == 2 {println!("{},{} {:?}", row_step, col_step, x)})
+            .take_while(|&(r, c)| r >= 0 && c >= 0)
+            .take_while(move |&(r, c)| r < h && c < w)
+            .map(|(r, c)| (r as usize, c as usize))
+            .map(move |(r, c)| self.get(r, c))
+    }
+}
+
+impl<T: Clone> Grid<T> {
+    pub fn like(&self, fill: T) -> Self {
+        Grid {
+            data: vec![fill; self.data.len()],
+            width: self.width,
+            height: self.height,
+        }
     }
 }
 
--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,11 +1,118 @@
+use common::ascii_enum;
+use common::grid::Grid;
 use common::input::Input;
 
+ascii_enum! {
+    Cell = Floor('.') | Free('L') | Full('#')
+}
+
 fn main() {
+    /*let input = Input::from_str(
+            "L.LL.LL.LL
+    LLLLLLL.LL
+    L.L.L..L..
+    LLLL.LL.LL
+    L.LL.LL.LL
+    L.LLLLL.LL
+    ..L.L.....
+    LLLLLLLLLL
+    L.LLLLLL.L
+    L.LLLLL.LL",
+        );*/
+
     let input = Input::from_file("data/day11-input.txt");
 
-    input.iter_lines()
-        .inspect(|x| println!("{:?}", x))
-        .collect::<Vec<_>>();
+    let start = Grid::<Cell>::from_input(input);
+    println!("{:?}", start);
 
-    println!("Hello, world!");
+    let mut front = start.clone();
+    let mut back = front.like(Cell::Floor);
+    while front != back {
+        simstep1(&front, &mut back);
+        Grid::swap(&mut front, &mut back);
+    }
+
+    let n_occupied = front.flat_iter().filter(|&&c| c == Cell::Full).count();
+    println!("Part 1: {}", n_occupied);
+
+    let mut front = start.clone();
+    let mut back = front.like(Cell::Floor);
+    while front != back {
+        simstep2(&front, &mut back);
+        Grid::swap(&mut front, &mut back);
+    }
+
+    let n_occupied = front.flat_iter().filter(|&&c| c == Cell::Full).count();
+    println!("Part 2: {}", n_occupied);
+}
+
+fn simstep1(front: &Grid<Cell>, back: &mut Grid<Cell>) {
+    for i in 0..front.height {
+        for j in 0..front.width {
+            let n = count_occupied_neighbors(i, j, front);
+            let out = match *front.get(i, j) {
+                Cell::Free if n == 0 => Cell::Full,
+                Cell::Full if n >= 4 => Cell::Free,
+                other => other,
+            };
+            back.set(i, j, out);
+        }
+    }
+}
+
+fn count_occupied_neighbors(i: usize, j: usize, grid: &Grid<Cell>) -> usize {
+    let i = i as isize;
+    let j = j as isize;
+
+    let neighbors = [
+        grid.safe_get(i - 1, j - 1),
+        grid.safe_get(i - 1, j),
+        grid.safe_get(i - 1, j + 1),
+        grid.safe_get(i, j - 1),
+        grid.safe_get(i, j + 1),
+        grid.safe_get(i + 1, j - 1),
+        grid.safe_get(i + 1, j),
+        grid.safe_get(i + 1, j + 1),
+    ];
+
+    neighbors
+        .iter()
+        .filter(|&&c| c == Some(&Cell::Full))
+        .count()
+}
+
+fn simstep2(front: &Grid<Cell>, back: &mut Grid<Cell>) {
+    for i in 0..front.height {
+        for j in 0..front.width {
+            let n = count_visibly_occupied(i, j, front);
+            let out = match *front.get(i, j) {
+                Cell::Free if n == 0 => Cell::Full,
+                Cell::Full if n >= 5 => Cell::Free,
+                other => other,
+            };
+            back.set(i, j, out);
+        }
+    }
+}
+
+fn count_visibly_occupied(i: usize, j: usize, grid: &Grid<Cell>) -> usize {
+    let vis = [
+        trace(i, j, -1, -1, &grid),
+        trace(i, j, -1, 0, &grid),
+        trace(i, j, -1, 1, &grid),
+        trace(i, j, 0, -1, &grid),
+        trace(i, j, 0, 1, &grid),
+        trace(i, j, 1, -1, &grid),
+        trace(i, j, 1, 0, &grid),
+        trace(i, j, 1, 1, &grid),
+    ];
+    vis.iter().filter(|&&c| c == Some(Cell::Full)).count()
+}
+
+fn trace(i: usize, j: usize, di: isize, dj: isize, grid: &Grid<Cell>) -> Option<Cell> {
+    grid.trace(i, j, di, dj)
+        .skip(1)
+        .filter(|&&c| c != Cell::Floor)
+        .next()
+        .copied()
 }
