ee3fb92f8e03fa79b9c4b0df33407d5f9dfc13ba
Martin Billinger <mbillingr@users.noreply.github.com>
2020-12-20 20:42:52 +0100

    prepare days 21-25

---
+++ day21/src/main.rs
@@ -0,0 +1,12 @@
+use common::input::Input;
+
+fn main() {
+    let input = Input::from_file("data/day21-input.txt");
+
+    let _ = input
+        .iter_lines()
+        .inspect(|x| println!("{:?}", x))
+        .collect::<Vec<_>>();
+
+    println!("Hello, world!");
+}
