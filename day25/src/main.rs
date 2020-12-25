df813865b3b988a36471e4268192502c8da013bc
Martin Billinger <mbillingr@users.noreply.github.com>
2020-12-25 08:41:12 +0100

    day 25

--- day25/src/main.rs
+++ day25/src/main.rs
@@ -1,12 +1,46 @@
 use common::input::Input;
 
+type Int = i64;
+
+const PUB_CARD: Int = 5290733;
+const PUB_DOOR: Int = 15231938;
+
+
 fn main() {
-    let input = Input::from_file("data/day25-input.txt");
 
-    let _ = input
-        .iter_lines()
-        .inspect(|x| println!("{:?}", x))
-        .collect::<Vec<_>>();
+    let mut value = 1;
+    let mut card_loop_size = 0;
+    while value != PUB_CARD {
+        value *= 7;
+        value %= 20201227;
+        card_loop_size += 1;
+    }
 
-    println!("Hello, world!");
+    println!("The card's loop size seems to be {}", card_loop_size);
+
+    let mut value = 1;
+    let mut door_loop_size = 0;
+    while value != PUB_DOOR {
+        value *= 7;
+        value %= 20201227;
+        door_loop_size += 1;
+    }
+
+    println!("The door's loop size seems to be {}", door_loop_size);
+
+    let mut value = 1;
+    for _ in 0..card_loop_size {
+        value *= PUB_DOOR;
+        value %= 20201227;
+    }
+
+    println!("Encryption key (card side): {}", value);
+
+    let mut value = 1;
+    for _ in 0..door_loop_size {
+        value *= PUB_CARD;
+        value %= 20201227;
+    }
+
+    println!("Encryption key (door side): {}", value);
 }
